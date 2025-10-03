bb022f371dd45df8c5484655ea30bc9deadcefda
Kyle-858 <krazykyle19@gmail.com>
2025-10-03 16:20:26 -0400

    Maxchar

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -7,7 +7,39 @@
  */
 
 const maxCharacter = (str) => {
-  
+    // OPTION 1:
+    // const charMap = {};
+    // let max = 0
+    // let maxChar = '';
+
+    // for (let char of str) {
+    //     charMap[char] = charMap[char] + 1 || 1;
+    // }
+
+    // for (let char in charMap) {
+    //     if (charMap[char] > max) {
+    //         max = charMap[char]
+    //         maxChar = char
+    //     }
+    // }
+    // return maxChar;
+
+    // OPTION 2:
+
+    const charMap = new Map()
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charMap.set(char, charMap.get(char) + 1 || 1)
+    }
+    for (const [char, count] of charMap) {
+        if (count > max) {
+            max = count;
+            maxChar = char;
+        }
+    }
+    return maxChar;
 };
 
 module.exports = maxCharacter;
